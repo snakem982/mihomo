527652802910c0e051a786f7d655c406425f6f7c
snakem982 <snakem982@gmail.com>
2026-06-19 14:08:51 +0800

    merge

--- adapter/outboundgroup/parser.go
+++ adapter/outboundgroup/parser.go
@@ -217,7 +217,7 @@ func ParseProxyGroup(config map[string]any, proxyMap map[string]C.Proxy, provide
 		return nil, fmt.Errorf("%w: The group [%s] with relay type was removed, please using dialer-proxy instead", errType, groupName)
 	case "smart":
 		opts := parseSmartOption(config)
-		return NewSmart(groupOption, emptyFallback, providers, opts...)
+		return NewSmart(&groupOption, emptyFallback, providers, opts...)
 	default:
 		return nil, fmt.Errorf("%w: %s", errType, groupOption.Type)
 	}
